cb1fa8e1a56448dba8174dab4955ecace87073ab
gua21150 <gua21150@uvg.edu.gt>
2024-11-11 01:47:37 -0600

    implementacion de deploy
    correcciones para el correcto funcionamiento

--- deploy.py
+++ deploy.py
@@ -2,12 +2,16 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Esto permite todas las solicitudes de cualquier origen
 
 # Cargar los modelos PCA y red neuronal
 pca_model = joblib.load('pca_model.pkl')
-nn_model = joblib.load('nn_model.pkl')
+nn_model = load_model('models\\NNmodel.h5')
 
 # Columnas
 numeric_columns = [
@@ -16,6 +20,7 @@ numeric_columns = [
     "TrainingTimesLastYear", "YearsAtCompany", "YearsInCurrentRole", 
     "YearsSinceLastPromotion", "YearsWithCurrManager"
 ]
+
 categorical_columns = [
     'BusinessTravel', 'Department', 'Education', 'EducationField', 
     'EnvironmentSatisfaction', 'Gender', 'JobInvolvement', 'JobLevel', 
@@ -51,30 +56,33 @@ def predict():
     # Separar datos numéricos y categóricos
     num_data = pd.DataFrame([data['numerical']], columns=numeric_columns)
     cat_data = pd.DataFrame([data['categorical']], columns=categorical_columns)
-    
+
     # Aplicar transformación PCA a datos numéricos y obtener solo las primeras dos componentes
     num_data = StandardScaler().fit_transform(num_data) 
     num_data_pca = pca_model.transform(num_data)[:, :2]
-    
-    # Dummificar las variables categóricas
-    cat_data_dummies = pd.get_dummies(cat_data)
+    # Obtener las variables numericas 
+    cat_data_dummies = pd.DataFrame(False, index=cat_data.index, columns=expected_columns)
 
-    # Asegurarse de que todas las columnas esperadas están presentes en el DataFrame de entrada
-    for col in expected_columns:
-        if col not in cat_data_dummies.columns:
-            cat_data_dummies[col] = 0
+    # Asegurarse de que todas las columnas esperadas están presentes en el DataFrame de entrada, igualando la estructura con la que se entrenó el modelo
+    for index, row in cat_data.iterrows():
+        for col in cat_data.columns:
+            column_name = f"{col}_{row[col]}"
+            if column_name in expected_columns:
+                cat_data_dummies.loc[index, column_name] = True
 
     # Reordenar las columnas para que coincidan con el modelo de entrenamiento
     cat_data_dummies = cat_data_dummies[expected_columns]
 
-    
     # Unir datos transformados
     full_data = pd.concat([pd.DataFrame(num_data_pca), cat_data_dummies], axis=1)
     
-    # Realizar predicción
-    prediction = nn_model.predict(full_data)
+    # Aplicar una segunda escalizacion a los datos
+    full_data = StandardScaler().transform(full_data)
     
-    return jsonify({'prediction': prediction.tolist()})
+    # Realizar predicción
+    prediction_prob = nn_model.predict(full_data)  # Esto devuelve una probabilidad
+    prediction_class = (prediction_prob[:, 1] >= 0.5).astype(int)  # Convierte en clase
+    return jsonify({'prediction': int(prediction_class[0])})
 
 if __name__ == '__main__':
     app.run()
